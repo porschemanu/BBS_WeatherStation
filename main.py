6adedcaaaee038ada1519286d616d0b2fc75b052
PorscheManu <53792937+ManuelYates@users.noreply.github.com>
2021-12-18 20:15:51 +0100

    First Running Version of Flask Side

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask , redirect,url_for, render_template
 
 app = Flask(__name__)
 
+humidity = 50
+parameters_name = []
+parameters = [1,2,5,3,4]
+
 @app.route("/")
 def home():
-    return "<h1>Test</h1>"
+    return render_template("index.html", test="55")
 
 if __name__ == "__main__":
     app.run()
+
+
