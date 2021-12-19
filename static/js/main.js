a6801ecf6d96ce19568079731bae70ddd0634ddf
PorscheManu <53792937+ManuelYates@users.noreply.github.com>
2021-12-19 21:25:12 +0100

    Continued Working on Dynamic Container

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,7 @@
-function fPie(humidity) {
-    var yValue = humidity;
+function fPie(value) {
     var xValues = ["Luftfeuchtigkeit"];
-    var yRestValue = 100 - yValue;
-    var yValues = [yRestValue, yValue];
+    var yRestValue = 100 - value;
+    var yValues = [yRestValue, value];
     var barColors = [
         "#111111",
         "#0000ff"
@@ -26,6 +25,22 @@ function fPie(humidity) {
 };
 
 
+function content_builder(entries_count,array_of_entries){
+    output = new String;
+
+    while(i < entries_count){
+        output += '<div>'
+        
+        
 
 
 
+        output += '</div>'
+        i++;
+    }
+
+    return output;
+}
+
+
+//Color change for temperatur
