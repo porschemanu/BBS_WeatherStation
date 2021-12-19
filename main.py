420be950c50aa80941428a0a36735a2d1676ae24
PorscheManu <53792937+ManuelYates@users.noreply.github.com>
2021-12-19 12:10:59 +0100

    Prototype kinda

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
-# field 1: temperatur, field 2: humidity, field 3: Luftdruck
+#AUTHOR: Manuel Yates
+#LAST UPDATED: 19.12.2021
+
+#NAMECONVENTIONS
+#   -Values | Einzelne Werte
+#       temperature         =   Temperatur              "Field1"
+#       humidity            =   Luftfeuchtigkeit        "Field2"
+#       airpressure         =   Luftdruck               "Field3"       
+#       last_update         =   Zuletzt aktualisiert    
+#   -Lists  | Listen
+#       list_temperature    =   Liste aller Temperatur-Daten
+#       list_humidity       =   Liste aller Luftfeutigkeit-Daten
+#       list_airpressure    =   Liste aller Luftdruck-Daten
+#       list_timecode       =   Liste aller Zeitstempel der Daten
+#       list_fieldname      =   Liste aller eingelesenen Werte
+#       
 
 from flask import Flask, redirect, url_for, render_template
 import json
 import datetime
 
-
-
 app = Flask(__name__)
 
-now = datetime.datetime.now()
-
-temperatur_list = []
-humidity_list = []
-pressure_list = []
+list_temperature    = []  
+list_humidity       = []  
+list_airpressure    = []  
+list_timecode       = []  
+list_fieldname      = []  
 
 def valueloader():
+    now = datetime.datetime.now()
+    print("Fetching Data from JSON")
     data = json.load(open('data/test.json'))
-    #print("Fetching Data from JSON")
-    listvalue = data['feeds']
-    
-    for x in listvalue:
-        temperatur_list.append(x['field1'])
-        humidity_list.append(x['field2'])
-        pressure_list.append(x['field3'])
+
+    print("Reading the number of data fields from the 'channel'")
+    list_channel = data['channel']
+
+    for data_entry in list_channel:
+        if "field" in data_entry:
+            list_fieldname.append(list_channel[data_entry])
+
+    #Sorting the input data fields to seperated lists
+    list_feed = data['feeds']
+
+    for data_entry in list_feed:
+        list_temperature.append(data_entry['field1'])
+        list_humidity.append(data_entry['field2'])
+        list_airpressure.append(data_entry['field3'])
+        list_timecode.append(data_entry['created_at'])
 
 @app.route("/")
 def home():
-    while now.hour < 24:
-        valueloader()
-        return render_template("index.html", humidity= humidity_list[-1], airpressure = pressure_list[-1], temperature = temperatur_list[-1])
-        sleep(5)
+    valueloader()
+    return render_template("index.html", 
+                            temperature = list_temperature[-1],
+                            humidity = list_humidity[-1],
+                            airpressure = list_airpressure[-1],
+                            last_update = list_timecode[-1],
+                            list_temperature = list_temperature,
+                            list_humidity = list_humidity,
+                            list_airpressure = list_airpressure,
+                            list_timecode = list_timecode,
+                            list_fieldname = list_fieldname)
 
 
 if __name__ == "__main__":
