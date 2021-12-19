3fdeac465e419f9db2486e605572e91d2be316ad
PorscheManu <53792937+ManuelYates@users.noreply.github.com>
2021-12-19 02:17:10 +0100

    Website is now kinda dynamic YAY

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from flask import Flask , redirect,url_for, render_template
+# field 1: temperatur, field 2: humidity, field 3: Luftdruck
+
+from flask import Flask, redirect, url_for, render_template
+import json
+import datetime
+
+
 
 app = Flask(__name__)
 
-humidity = 50
-parameters_name = []
-parameters = [1,2,5,3,4]
+now = datetime.datetime.now()
+
+temperatur_list = []
+humidity_list = []
+pressure_list = []
+
+def valueloader():
+    data = json.load(open('data/test.json'))
+    #print("Fetching Data from JSON")
+    listvalue = data['feeds']
+    
+    for x in listvalue:
+        temperatur_list.append(x['field1'])
+        humidity_list.append(x['field2'])
+        pressure_list.append(x['field3'])
 
 @app.route("/")
 def home():
-    return render_template("index.html", test="55")
+    while now.hour < 24:
+        valueloader()
+        return render_template("index.html", humidity= humidity_list[-1], airpressure = pressure_list[-1], temperature = temperatur_list[-1])
+        sleep(5)
+
 
 if __name__ == "__main__":
     app.run()
-
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,25 @@
-function fPie(humidity){
+function fPie(humidity) {
     var yValue = humidity;
     var xValues = ["Luftfeuchtigkeit"];
     var yRestValue = 100 - yValue;
-    var yValues = [yRestValue,yValue];
+    var yValues = [yRestValue, yValue];
     var barColors = [
-      "#111111",
-      "#0000ff"
+        "#111111",
+        "#0000ff"
     ];
-    
+
     new Chart("myChart", {
         type: 'doughnut',
         data: {
             datasets: [{
-            backgroundColor: barColors,
-            data: yValues
+                backgroundColor: barColors,
+                data: yValues
             }]
         },
         options: {
             plugins: {
-            legend: false,
-            tooltip: false,
+                legend: false,
+                tooltip: false,
             }
         }
     });
